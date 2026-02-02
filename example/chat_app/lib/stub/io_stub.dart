8042bf01dca4da398793a4002ae8a2127dfb563f
Jhin Lee <leehack@gmail.com>
2026-02-01 21:51:32 -0500

    fix: update io_stub to support web compilation of model_service

--- example/chat_app/lib/stub/io_stub.dart
+++ example/chat_app/lib/stub/io_stub.dart
@@ -7,6 +7,8 @@ class Platform {
   static bool get isWindows => false;
 }
 
+enum FileMode { read, write, append, writeOnly, writeOnlyAppend }
+
 class File {
   File(String path);
   String get path => '';
@@ -14,6 +16,22 @@ class File {
   int lengthSync() => 0;
   Future<void> delete() async {}
   void deleteSync() {}
+  Future<List<String>> readAsLines() async => [];
+  Future<File> create({bool recursive = false}) async => this;
+  Future<RandomAccessFile> open({FileMode mode = FileMode.read}) async =>
+      RandomAccessFile();
+  Future<File> writeAsString(
+    String contents, {
+    FileMode mode = FileMode.write,
+    bool flush = false,
+  }) async => this;
+}
+
+class RandomAccessFile {
+  Future<void> truncate(int length) async {}
+  Future<void> setPosition(int position) async {}
+  Future<void> writeFrom(List<int> buffer, [int start = 0, int? end]) async {}
+  Future<void> close() async {}
 }
 
 class Directory {
@@ -21,4 +39,5 @@ class Directory {
   String get path => '';
   bool existsSync() => false;
   void createSync({bool recursive = false}) {}
+  Future<void> create({bool recursive = false}) async {}
 }
